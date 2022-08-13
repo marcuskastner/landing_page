375a8aaa8b156b6ce7034ffa5de4ede604e52ab3
Marcus Kastner <marcushkastner@gmail.com>
2022-08-13 09:33:49 -0700

    organized icon paths

--- web/src/components/atom/logos.js
+++ web/src/components/atom/logos.js
@@ -1,7 +1,7 @@
-import CSS from "../../images/css-3.svg"
-import HTML from "../../images/html.svg"
-import JS from "../../images/js.svg"
-import REACT from "../../images/react.svg"
-import GATSBY from "../../images/gatsby.svg"
-import XD from "../../images/xd.svg"
+import CSS from "../../images/icons/css-3.svg"
+import HTML from "../../images/icons/html.svg"
+import JS from "../../images/icons/js.svg"
+import REACT from "../../images/icons/react.svg"
+import GATSBY from "../../images/icons/gatsby.svg"
+import XD from "../../images/icons/xd.svg"
 export const icons = [CSS, HTML, JS, REACT, GATSBY, XD]
