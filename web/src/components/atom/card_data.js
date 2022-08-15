de43cbb7277f0c1276058720569d5e49dc83f65f
Marcus Kastner <marcushkastner@gmail.com>
2022-08-14 17:48:00 -0700

    changed card data

--- web/src/components/atom/card_data.js
+++ web/src/components/atom/card_data.js
@@ -7,7 +7,7 @@ export const card_data = [
     title: "There Alongside",
     description:
       "I am currently contracted to build the web platform for There Alongside, a digital design brand. The site uses Gatsby on the front end and Sanity on the back end. This makes it easy and convienient for my client to add information to about the artists they represent. I collaborate weekly with my client to develop the desired UI/ UX from storyboards. My client wanted complicated animations as part of their user experience. To accomplish this I have been utiliing framer-motion. ",
-    tech: [0, 1, 2],
+    tech: [4, 6, 7],
     background: work3,
   },
 
@@ -15,17 +15,17 @@ export const card_data = [
     title: "Ecommerce Site",
     description:
       "This site could be used as a general template for any ecommerce application. It uses Gatsby on the front end and Sanity on the back end to pull all product information. Making it extremely easy for clients to add or edit items in their store. Furthermore, the site is fully integrated with stripe to securely handle payments. I think the biggest accomplishment for myself with this project was reading Gatsby, Sanity and Stripe's documentation to implement their API's. Furthermore, I took on the role of UI designer using XD to build user stories.",
-    tech: [0, 1, 2],
+    tech: [4, 5, 9],
     background: work1,
-    github: "https://github.com/marcuskastner/climbingECommerce",
-    link: "https://marcus-kastner-climbing-commerce.netlify.app/",
+    github: "https://github.com/marcuskastner/eCommerce",
+    link: "https://marcus-kastner-ecommerce.netlify.app/",
   },
 
   {
     title: "React Native App",
     description:
       "This is a react native marketplace app. Furthermore, the app is integrated with Firebase Auth service to handle secure client login. Similarly to my other projects this required me to read through documentation and solve my own problems as they arise. I think this project shows I am interested in many different types of front end technologies and am constatly pushing myself to learn more.",
-    tech: [4, 5, 3],
+    tech: [3, 8],
     background: work2,
     github: "https://github.com/marcuskastner/nativeApp",
     link: "https://marcus-kastner-native-app.netlify.app/",
--- web/src/components/atom/logos.js
+++ web/src/components/atom/logos.js
@@ -4,4 +4,20 @@ import JS from "../../images/icons/js.svg"
 import REACT from "../../images/icons/react.svg"
 import GATSBY from "../../images/icons/gatsby.svg"
 import XD from "../../images/icons/xd.svg"
-export const icons = [CSS, HTML, JS, REACT, GATSBY, XD]
+import SANITY from "../../images/icons/sanity.png"
+import MOTION from "../../images/icons/motion.png"
+import FIREBASE from "../../images/icons/firebase.svg"
+import STRIPE from "../../images/icons/stripe.png"
+
+export const icons = [
+  CSS,
+  HTML,
+  JS,
+  REACT,
+  GATSBY,
+  XD,
+  SANITY,
+  MOTION,
+  FIREBASE,
+  STRIPE,
+]
